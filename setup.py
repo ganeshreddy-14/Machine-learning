5097a9133691fe5af53084fa8646593e7c49ce9d
ganeshreddy-14 <edulaganeshredd2005@gmail.com>
2025-08-09 11:31:58 +0530

    third commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+setup(
+name='mlproject',
+version='0.0.1',
+author='soahil',
+author_email='sohail007@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
